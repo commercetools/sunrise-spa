5c569c6d2d672320ae77ebd7854fcd89bf9f3de9
Laura Luiz <laura.luiz@commercetools.de>
2018-05-22 16:40:22 +0200

    Add apollo

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
+import { apolloProvider } from './vue-apollo'
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App),
+  provide: apolloProvider.provide(),
+  render: h => h(App)
 }).$mount('#app');
