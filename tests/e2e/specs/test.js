e11c8ab0f226f66b54bece5db31fef3796b35cb6
Zeina Idris <zeina.idris@commercetools.de>
2018-06-25 17:30:24 +0200

    test e2e with cypress

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,10 +1,31 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe('My first test', () => {
+describe('Categories navigation', () => {
   beforeEach(() => {
     cy.visit('/');
   });
-  it('Check categories children', () => {
-    cy.get('#navigation ul.categories-1st-level > li:nth-child(2) > a');
+
+  it('links to 1st level categories', () => {
+    cy.get('#nav-categories-menu > li')
+      .contains('Women')
+      .should('have.attr', 'href', '#/products/women');
+  });
+
+  it('links to 2nd level category', () => {
+    cy.get('#nav-categories-menu > li')
+      .contains('Women')
+      .parent()
+      .trigger('mouseover')
+      .contains('Shoes')
+      .should('have.attr', 'href', '#/products/women-shoes');
+  });
+
+  it('links to 3rd level category', () => {
+    cy.get('#nav-categories-menu > li')
+      .contains('Women')
+      .parent()
+      .trigger('mouseover')
+      .contains('Ankle boots')
+      .should('have.attr', 'href', '#/products/women-shoes-ankle-boots');
   });
 });
