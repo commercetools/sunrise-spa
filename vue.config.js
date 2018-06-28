ee087b5224e9964473af5e8f278bc91768db25fb
Laura Luiz <laura.luiz@commercetools.de>
2018-06-28 17:53:57 +0200

    Fix vue-i18n by using its plugin

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,5 @@
-const merge = require('deepmerge');
-
 module.exports = {
-  chainWebpack: (config) => {
-    config.module
-      .rule('vue')
-      .use('vue-loader')
-      .tap(options =>
-        merge(options, {
-          loaders: {
-            i18n: '@kazupon/vue-i18n-loader',
-          },
-        }));
+  pluginOptions: {
+    enableInSFC: true,
   },
 };
