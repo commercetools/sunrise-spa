dc945aba523dc0a229ff5f97aad9e5ad9fcffa55
Laura Luiz <laura.luiz@commercetools.de>
2018-07-04 17:32:42 +0200

    Remove console messages

--- src/store/modules/categories.js
+++ src/store/modules/categories.js
@@ -3,14 +3,6 @@ import fetchAllCategoriesQuery from '@/graphql/FetchAllCategories.gql';
 
 const SET_CATEGORIES = 'setCategories';
 
-function extractCategories(response) {
-  const result = response.data.categories;
-  if (result.total > result.count) {
-    console.warn(`Categories limited to ${result.count}, increase the limit if you want to fetch all ${result.total}.`);
-  }
-  return result.results;
-}
-
 export default {
   state: {
     items: [],
@@ -29,11 +21,10 @@ export default {
             locale,
           },
         }).then((response) => {
-          const categories = extractCategories(response);
+          const categories = response.data.categories.results;
           context.commit(SET_CATEGORIES, categories);
           resolve();
         }).catch((error) => {
-          console.error('Could not fetch categories.', error);
           reject(error);
         });
       });
@@ -43,7 +34,6 @@ export default {
   mutations: {
     [SET_CATEGORIES](state, categories) {
       state.items = categories;
-      console.log(`Set category tree with ${categories.length} root categories.`);
     },
   },
 };
