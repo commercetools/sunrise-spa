b705d12aeadef27f0b5072ebd6185e9f72b95bc4
Laura Luiz <laura.luiz@commercetools.de>
2018-07-10 18:11:13 +0200

    Increase allowed asset and entrypoint sizes to remove warnings

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,10 @@ module.exports = {
   pluginOptions: {
     enableInSFC: true,
   },
+  configureWebpack: {
+    performance: {
+      maxEntrypointSize: 500000,
+      maxAssetSize: 350000,
+    },
+  },
 };
