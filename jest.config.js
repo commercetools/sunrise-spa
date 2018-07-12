247e96619243ca0b9b058c67df08ee6c488eb86d
Laura Luiz <laura.luiz@commercetools.de>
2018-07-12 14:28:29 +0200

    Fix jest issue with babel

--- jest.config.js
+++ jest.config.js
@@ -19,4 +19,7 @@ module.exports = {
   testMatch: [
     '<rootDir>/(tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx))',
   ],
+  setupFiles: [
+    'jest-localstorage-mock',
+  ],
 };
