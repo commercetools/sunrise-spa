7fc511e66af906b4819164a49bed752ddf4b35fb
Laura Luiz <laura.luiz@commercetools.de>
2018-07-16 17:40:21 +0200

    Stub selectboxit in location selector spec

--- tests/unit/LocationSelector.spec.js
+++ tests/unit/LocationSelector.spec.js
@@ -6,15 +6,17 @@ import SelectBoxIt from '@/components/global/SelectBoxIt.vue';
 Vue.component('SelectBoxIt', SelectBoxIt);
 
 describe('LocationSelector.vue', () => {
-  it('renders props.msg when passed', () => {
-    // const msg = 'new message';
+  it('displays languages defined in configuration', () => {
     const wrapper = shallowMount(LocationSelector, {
       mocks: {
         $i18n: ({ locale: 'en' }),
         $sunrise: ({ languages: { en: 'English', de: 'Deutsch' } }),
         $t: () => {},
       },
+      stubs: {
+        SelectBoxIt: '<div/>',
+      },
     });
-    console.log(wrapper);
+    console.log(wrapper.find(SelectBoxIt).props());
   });
 });
