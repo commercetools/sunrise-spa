f413073a980227ba3c79ceba1f549595c573b02a
Laura Luiz <laura.luiz@commercetools.de>
2018-07-26 13:46:05 +0200

    Filter products by selected category

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,12 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import categories from '@/store/modules/categories';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
-
+    categories,
   },
 
   state: {
