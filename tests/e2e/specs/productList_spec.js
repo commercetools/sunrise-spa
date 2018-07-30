781e50f2a9b6f60793bbf4c3c135b7f722daf499
Laura Luiz <laura.luiz@commercetools.de>
2018-07-30 14:53:49 +0200

    Test products without discount in e2e

--- tests/e2e/specs/productList_spec.js
+++ tests/e2e/specs/productList_spec.js
@@ -3,7 +3,7 @@ describe('Product list', () => {
     cy.visit('/#/products/women-shoes-ankle-boots/');
   });
 
-  it('displays products information', () => {
+  it('displays product information', () => {
     cy.get('[data-test=product-thumbnail-name]', { timeout: 10000 })
       .contains('Booties Lemare grey')
       .parentsUntil('[data-test=product-thumbnail]')
@@ -15,12 +15,34 @@ describe('Product list', () => {
           .should('include', '.jpg');
 
         cy.wrap($thumbnail)
-          .find('[data-test=product-thumbnail-original-price]')
+          .find('[data-test=product-thumbnail-sale-flag]')
+          .should('exist');
+
+        cy.wrap($thumbnail)
+          .find('[data-test=product-thumbnail-price]')
           .should('contain', '248,75 €');
 
         cy.wrap($thumbnail)
           .find('[data-test=product-thumbnail-discounted-price]')
           .should('contain', '174,12 €');
+      });
+
+    cy.get('[data-test=product-thumbnail-name]')
+      .contains('Booties Lemare black')
+      .parentsUntil('[data-test=product-thumbnail]')
+      .parent()
+      .then(($thumbnail) => {
+        cy.wrap($thumbnail)
+          .find('[data-test=product-thumbnail-sale-flag]')
+          .should('not.exist');
+
+        cy.wrap($thumbnail)
+          .find('[data-test=product-thumbnail-price]')
+          .should('contain', '231,25 €');
+
+        cy.wrap($thumbnail)
+          .find('[data-test=product-thumbnail-discounted-price]')
+          .should('not.exist');
       });
   });
 });
