3616c27f001f8ab338cd2b3d17ad3d3333bdbf7f
Laura Luiz <laura.luiz@commercetools.de>
2018-07-31 16:18:48 +0200

    Unit test productThumbnail

--- tests/unit/jest.init.js
+++ tests/unit/jest.init.js
@@ -3,5 +3,11 @@ import VueTestUtils from '@vue/test-utils';
 VueTestUtils.config.mocks = {
   $i18n: {},
   $t: jest.fn(),
+  $n: jest.fn(),
   $sunrise: {},
+  $store: {
+    state: {
+      country: 'de-DE',
+    },
+  },
 };
