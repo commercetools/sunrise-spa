a4722df91d5b03bae7397cfb5e034ec4da9633f7
Laura Luiz <laura.luiz@commercetools.de>
2018-08-01 11:35:02 +0200

    Finish unit testing ProductList component

--- tests/unit/specs/components/ProductList.spec.js
+++ tests/unit/specs/components/ProductList.spec.js
@@ -20,11 +20,47 @@ describe('ProductList.vue', () => {
     const wrapper = shallowMount(ProductList, {
       propsData: { categorySlug: 'some-category-slug' },
       methods: { categoryBySlug },
-      stubs: { 'router-link': '<a/>' },
     });
 
     // eslint-disable-next-line no-unused-vars
     const unused = wrapper.vm.category;
     expect(categoryBySlug).toHaveBeenCalledWith('some-category-slug');
+  });
+
+  it('calls ProductThumbnail for each obtained product', () => {
+    const wrapper = shallowMount(ProductList, {
+      methods: { categoryBySlug },
+      stubs: { ProductThumbnail: '<div data-test="product-thumbnail"/>' },
+    });
+    wrapper.setData({
+      products: {
+        results: [
+          { id: 'product-id-1' },
+          { id: 'product-id-2' },
+          { id: 'product-id-3' },
+          { id: 'product-id-4' },
+        ],
+      },
+    });
+
+    expect(wrapper.findAll('[data-test=product-thumbnail]').length).toBe(4);
+  });
+
+  describe('when building a GraphQL request', () => {
+    const filterByCategoryPredicate = 'masterData(current(categories(id="some-category-id")))';
+
+    it('does not generate a predicate to filter by category when category is missing ', () => {
+      categoryBySlug = () => null;
+      const wrapper = shallowMount(ProductList, { methods: { categoryBySlug } });
+
+      expect(wrapper.vm.gqlPredicate).toBeNull();
+    });
+
+    it('generates a predicate to filter by category when a category exists', () => {
+      categoryBySlug = () => ({ id: 'some-category-id' });
+      const wrapper = shallowMount(ProductList, { methods: { categoryBySlug } });
+
+      expect(wrapper.vm.gqlPredicate).toContain(filterByCategoryPredicate);
+    });
   });
 });
