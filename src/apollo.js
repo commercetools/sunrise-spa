859e608a19b43d5c0b757e5af28d5cc70a20f5b0
Laura Luiz <laura.luiz@commercetools.de>
2018-08-03 14:38:42 +0200

    Enable log in/out from ct with apollo

--- src/apollo.js
+++ src/apollo.js
@@ -1,73 +1,92 @@
-import { createAuthMiddlewareForClientCredentialsFlow }
+import { createAuthMiddlewareForClientCredentialsFlow, createAuthMiddlewareForPasswordFlow }
   from '@commercetools/sdk-middleware-auth/dist/commercetools-sdk-middleware-auth.cjs';
 import Vue from 'vue';
 import VueApollo from 'vue-apollo';
-import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache, IntrospectionFragmentMatcher } from 'apollo-cache-inmemory';
 import { setContext } from 'apollo-link-context';
-import { createApolloClient } from 'vue-cli-plugin-apollo/graphql-client';
+import { createApolloClient, restartWebsockets } from 'vue-cli-plugin-apollo/graphql-client';
 import config from '@/../sunrise.config';
+import introspectionQueryResultData from '@/../graphql-fragments.json';
 
 // Install the vue plugin
 Vue.use(VueApollo);
 
-const httpEndpoint = process.env.VUE_APP_GRAPHQL_HTTP || `${config.ct.api.host}/${config.ct.auth.projectKey}/graphql`;
-
-// Create commercetools authentication middleware
-const authMiddleware = createAuthMiddlewareForClientCredentialsFlow(config.ct.auth);
-
-function addAuthHeader(request) {
-  return new Promise(success => authMiddleware(requestWithAuth => success(requestWithAuth))(request));
-}
-
-const link = setContext(request => addAuthHeader(request)).concat(new HttpLink({ uri: httpEndpoint }));
+// Matcher for fragments on unions and interfaces
+const fragmentMatcher = new IntrospectionFragmentMatcher({ introspectionQueryResultData });
 
 // Config
 const defaultOptions = {
-  // You can use `https` for secure connection (recommended in production)
-  httpEndpoint,
-  // You can use `wss` for secure connection (recommended in production)
-  // Use `null` to disable subscriptions
-  wsEndpoint: null,
-  // Enable Automatic Query persisting with Apollo Engine
-  persisting: false,
-  // Use websockets for everything (no HTTP)
-  // You need to pass a `wsEndpoint` for this to work
-  websocketsOnly: false,
-  // Is being rendered on the server?
-  ssr: false,
-
-  // Override default http link
-  link,
-
-  // Override default cache
-  // cache: myCache
-
-  // Additional ApolloClient options
-  // apollo: { ... }
-
-  // Client local data (see apollo-link-state)
-  // clientState: { resolvers: { ... }, defaults: { ... } }
+  httpEndpoint: process.env.VUE_APP_GRAPHQL_HTTP || `${config.ct.api.host}/${config.ct.auth.projectKey}/graphql`,
+  cache: new InMemoryCache({ fragmentMatcher }),
 };
 
+// Create commercetools authentication middlewares
+const clientCredentialsFlowMiddleware = createAuthMiddlewareForClientCredentialsFlow(config.ct.auth);
+let passwordFlowMiddleware;
+
+function createAuthLink(getAuthMiddleware) {
+  return setContext((_, prevContext) => {
+    const authMiddleware = getAuthMiddleware();
+    return new Promise((success, reject) => {
+      if (authMiddleware) {
+        authMiddleware(newContext => success(newContext))(prevContext);
+      } else {
+        reject(new Error('Could not authenticate, probably you are not logged in'));
+      }
+    });
+  });
+}
+
 export default function createProvider(options = {}) {
-  // Create apollo client
-  const { apolloClient, wsClient } = createApolloClient({
+  const defaultClient = createApolloClient({
     ...defaultOptions,
     ...options,
-  });
-  apolloClient.wsClient = wsClient;
+    link: createAuthLink(() => clientCredentialsFlowMiddleware),
+  }).apolloClient;
+
+  const meClient = createApolloClient({
+    ...defaultOptions,
+    ...options,
+    link: createAuthLink(() => passwordFlowMiddleware),
+  }).apolloClient;
 
   // Create vue apollo provider
   return new VueApollo({
-    defaultClient: apolloClient,
-    defaultOptions: {
-      $query: {
-        // fetchPolicy: 'cache-and-network',
-      },
+    defaultClient,
+    clients: {
+      me: meClient,
     },
     errorHandler(error) {
       // eslint-disable-next-line no-console
       console.error(error.message);
     },
   });
+}
+
+export async function onLogin(apolloClient, username, password) {
+  passwordFlowMiddleware = createAuthMiddlewareForPasswordFlow({
+    ...config.ct.auth,
+    credentials: {
+      ...config.ct.auth.credentials,
+      user: { username, password },
+    },
+  });
+  if (apolloClient.wsClient) restartWebsockets(apolloClient.wsClient);
+  try {
+    await apolloClient.resetStore();
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error('%cError on cache reset (login)', 'color: orange;', e.message);
+  }
+}
+
+export async function onLogout(apolloClient) {
+  passwordFlowMiddleware = null;
+  if (apolloClient.wsClient) restartWebsockets(apolloClient.wsClient);
+  try {
+    await apolloClient.resetStore();
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error('%cError on cache reset (logout)', 'color: orange;', e.message);
+  }
 }
