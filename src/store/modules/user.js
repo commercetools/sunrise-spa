ec967cb888011d0ea9ed16e91d058de0052f35bc
Laura Luiz <laura.luiz@commercetools.de>
2018-08-17 13:57:24 +0200

    Add profile page to properly finish user flow

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,17 +1,17 @@
 import gql from 'graphql-tag';
 import { apolloProvider } from '@/main';
 
-const SET_IS_LOGGED_IN = 'SET_IS_LOGGED_IN';
 const SET_INFO = 'SET_INFO';
 
 export default {
   state: {
-    isLoggedIn: false,
-    info: {},
+    info: null,
   },
 
   getters: {
+    user: state => state.info || {},
 
+    isAuthenticated: state => !!state.info,
   },
 
   actions: {
@@ -22,7 +22,10 @@ export default {
             customerSignMeIn(draft: $draft) {
               customer {
                 email
+                title
                 firstName
+                lastName
+                customerNumber
               }
             }
           }`,
@@ -31,23 +34,17 @@ export default {
         },
       }).then((response) => {
         apolloProvider.defaultClient.login(email, password);
-        commit(SET_IS_LOGGED_IN, true);
         commit(SET_INFO, response.data.customerSignMeIn.customer);
       }),
 
     logout: ({ commit }) =>
       apolloProvider.defaultClient.logout()
         .then(() => {
-          commit(SET_IS_LOGGED_IN, false);
-          commit(SET_INFO, {});
+          commit(SET_INFO, null);
         }),
   },
 
   mutations: {
-    [SET_IS_LOGGED_IN](state, isLoggedIn) {
-      state.isLoggedIn = isLoggedIn;
-    },
-
     [SET_INFO](state, customer) {
       state.info = customer;
     },
---
+++ src/views/MyAccountPage.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="my-account">
+    <div class="container">
+      <div class="my-account-title">
+        <span class="my-account-title-text icon-user">{{ $t('title') }}</span>
+      </div>
+      <div class="row">
+        <div class="col-sm-9">
+          <div class="my-account-content">
+            <UserProfile/>
+          </div>
+        </div>
+        <div class="col-sm-3">
+          <div class="my-account-banners">
+            <div class="banner-one-wrapper hidden-xs">
+              <div class="my-account-banner">
+                <div class="banner-title">
+                  {{ $t('bannerOne.title') }}
+                </div>
+                <hr class="banner-hr">
+                <div class="banner-subtitle">
+                  {{ $t('bannerOne.subtitle') }}
+                </div>
+                <div class="banner-btn-wrapper">
+                  <!--<a class="banner-btn" href="{{content.bannerOne.url}}">-->
+                    <!--{{ $t('bannerOne.link') }}-->
+                  <!--</a>-->
+                </div>
+              </div>
+            </div>
+            <div class="banner-two-wrapper">
+              <div class="my-account-banner image-banner">
+                <div class="banner-title">
+                  {{ $t('bannerTwo.title') }}
+                </div>
+                <hr class="banner-hr">
+                <div class="banner-subtitle">
+                  {{ $t('bannerTwo.subtitle') }}
+                </div>
+                <div class="banner-btn-wrapper">
+                  <!--<a class="banner-btn" href="{{content.bannerTwo.url}}">-->
+                    <!--{{ $t('bannerTwo.link') }}-->
+                  <!--</a>-->
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import UserProfile from '@/components/UserProfile.vue';
+
+export default {
+  name: 'MyAccount',
+  components: { UserProfile },
+};
+</script>
+
+<i18n>
+{
+  "en": {
+    "title": "My Account",
+    "bannerOne": {
+      "title": "Your Sunrise",
+      "subtitle": "Make the great days happen",
+      "link": "Shop All"
+    },
+    "bannerTwo": {
+      "title": "Your Sunrise",
+      "subtitle": "Make the great days happen",
+      "link": "Shop All"
+    }
+  },
+  "de": {
+    "title": "Mein Konto",
+    "bannerOne": {
+      "title": "Dein Sunrise",
+      "subtitle": "Urlaub in der Sonne",
+      "link": "Alles bestellen"
+    },
+    "bannerTwo": {
+      "title": "Sunrise Shop",
+      "subtitle": "Neue Saison, neuer Style",
+      "link": "Alles bestellen"
+    }
+  }
+}
+</i18n>
