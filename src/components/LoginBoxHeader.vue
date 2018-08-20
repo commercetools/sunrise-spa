ee46c490865bfb6e4adfecbd2535da296c784de3
Laura Luiz <laura.luiz@commercetools.de>
2018-08-20 11:55:46 +0200

    Keep route on logout and use meta for indicating requireAuth

--- src/components/LoginBoxHeader.vue
+++ src/components/LoginBoxHeader.vue
@@ -42,7 +42,9 @@ export default {
   methods: {
     logout() {
       this.$store.dispatch('logout')
-        .then(() => this.$router.push('/'));
+        .then(() => {
+          this.$router.replace({ query: { logout: true } });
+        });
     },
   },
 };
