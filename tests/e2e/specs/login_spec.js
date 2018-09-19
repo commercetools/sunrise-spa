488fa84ff90e1e4aed01d4a0018823cb70a854ed
Laura Luiz <laura.luiz@commercetools.de>
2018-09-19 17:24:55 +0200

    Extract signup and logout into Cy command

--- tests/e2e/specs/login_spec.js
+++ tests/e2e/specs/login_spec.js
@@ -1,21 +1,28 @@
-describe('Login box', () => {
+import { randomCustomer } from '../support/utils';
+
+describe('Login', () => {
+  const customer = randomCustomer();
+
   before(() => {
-    cy.visit('/');
+    cy.signup(customer);
+    cy.logout();
+    cy.visit('/login');
   });
 
   it('logs in', () => {
     cy.get('[data-test=login-button]').click();
-
-    cy.get('[data-test=login-form-email]').type('willy.wonka@commercetools.com');
-    cy.get('[data-test=login-form-password]').type('p@ssword');
+    cy.get('[data-test=login-form-email]').type(customer.email);
+    cy.get('[data-test=login-form-password]').type(customer.password);
     cy.get('[data-test=login-form-submit]').click();
 
-    cy.get('[data-test=login-info-name]').should('contain', 'Willy');
+    cy.get('[data-test=user-profile-name]').should('contain', `${customer.firstName} ${customer.lastName}`);
+    cy.get('[data-test=user-profile-email]').should('contain', customer.email);
+
+    cy.get('[data-test=login-info-name]').should('contain', customer.firstName);
     cy.get('[data-test=login-button]').should('not.exist');
     cy.get('[data-test=logout-button]').should('exist');
 
-    cy.get('[data-test=logout-button]').click();
-
+    cy.logout();
     cy.get('[data-test=login-button]').should('exist');
     cy.get('[data-test=logout-button]').should('not.exist');
     cy.get('[data-test=login-info-name]').should('not.exist');
