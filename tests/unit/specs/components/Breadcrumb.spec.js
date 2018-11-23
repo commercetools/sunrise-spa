8546643c78a2480528c0bf52ab3e419aa88260f9
Laura Luiz <laura.luiz@commercetools.de>
2018-11-23 15:14:40 +0100

    Fix some tests

--- tests/unit/specs/components/Breadcrumb.spec.js
+++ tests/unit/specs/components/Breadcrumb.spec.js
@@ -2,13 +2,17 @@ import { shallowMount } from '@vue/test-utils';
 import Breadcrumb from '@/components/Breadcrumb.vue';
 
 describe('Breadcrumb.vue', () => {
+  const someCategory = {
+    id: 'some-id',
+    ancestors: [],
+  };
+
   let options;
 
   beforeEach(() => {
     options = {
-      methods: { categoryBySlug: jest.fn() },
       mocks: { $t: jest.fn() },
-      stubs: { 'router-link': '<a/>' },
+      stubs: { 'router-link': true },
     };
   });
 
@@ -16,26 +20,20 @@ describe('Breadcrumb.vue', () => {
     expect(shallowMount(Breadcrumb, options).isVueInstance()).toBeTruthy();
   });
 
-  it('hides when there is no category information', () => {
-    options.methods.categoryBySlug = () => null;
+  it('obtains category', () => {
     const wrapper = shallowMount(Breadcrumb, options);
+    wrapper.setData({
+      categories: {
+        results: [],
+      },
+    });
+    expect(wrapper.vm.category).toBeUndefined();
 
-    expect(wrapper.vm.active).toBeFalsy();
-  });
-
-  it('shows when there is category information', () => {
-    options.methods.categoryBySlug = () => ({ });
-    const wrapper = shallowMount(Breadcrumb, options);
-
-    expect(wrapper.vm.active).toBeTruthy();
-  });
-
-  it('obtains corresponding category information', () => {
-    const wrapper = shallowMount(Breadcrumb, options);
-    wrapper.setProps({ categorySlug: 'some-category-slug' });
-
-    // eslint-disable-next-line no-unused-vars
-    const unused = wrapper.vm.category;
-    expect(options.methods.categoryBySlug).toHaveBeenCalledWith('some-category-slug');
+    wrapper.setData({
+      categories: {
+        results: [someCategory],
+      },
+    });
+    expect(wrapper.vm.category).toEqual(someCategory);
   });
 });
