be0e75bdb9dec3c8b079ceee509c5518f533a68c
Laura Luiz <laura.luiz@commercetools.de>
2018-11-28 13:31:47 +0100

    Test that on reload the customer stay logged in

--- tests/e2e/specs/login_spec.js
+++ tests/e2e/specs/login_spec.js
@@ -10,20 +10,27 @@ describe('Login', () => {
     cy.visit('/login');
   });
 
+  function shouldBeLoggedIn() {
+    cy.location('pathname').should('eq', '/user');
+    cy.get('[data-test=login-info-name]').should('contain', customer.firstName);
+    cy.get('[data-test=login-button]').should('not.exist');
+    cy.get('[data-test=logout-button]').should('exist');
+    cy.get('[data-test=user-profile-name]').should('contain', `${customer.firstName} ${customer.lastName}`);
+    cy.get('[data-test=user-profile-email]').should('contain', customer.email);
+  }
+
   it('logs in', () => {
     cy.get('[data-test=login-button]').click();
     cy.get('[data-test=login-form-email]').type(customer.email);
     cy.get('[data-test=login-form-password]').type(customer.password);
     cy.get('[data-test=login-form-submit]').click();
+    shouldBeLoggedIn();
 
-    cy.get('[data-test=user-profile-name]').should('contain', `${customer.firstName} ${customer.lastName}`);
-    cy.get('[data-test=user-profile-email]').should('contain', customer.email);
-
-    cy.get('[data-test=login-info-name]').should('contain', customer.firstName);
-    cy.get('[data-test=login-button]').should('not.exist');
-    cy.get('[data-test=logout-button]').should('exist');
+    cy.reload();
+    shouldBeLoggedIn();
 
     cy.logout();
+    cy.location('pathname').should('eq', '/login');
     cy.get('[data-test=login-button]').should('exist');
     cy.get('[data-test=logout-button]').should('not.exist');
     cy.get('[data-test=login-info-name]').should('not.exist');
