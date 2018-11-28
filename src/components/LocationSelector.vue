29bfbaa146e086b76eacc167cffe07fe00673263
Laura Luiz <laura.luiz@commercetools.de>
2018-11-28 11:32:23 +0100

    Fix location selector dropdown

--- src/components/LocationSelector.vue
+++ src/components/LocationSelector.vue
@@ -2,40 +2,49 @@
   <li v-if="active"
       data-test="location-selector"
       class="list-item-location clearfix">
-    <button data-test="location-selector-open-button"
-            id="location-dropdown-toggle-btn"
-            class="location-dropdown-toggle">
+    <button @click="toggle = !toggle"
+            data-test="location-selector-open-button">
       <img class="pull-right"
            src="../assets/img/globe-2.png"
            :alt="$t('main.header.location')">
     </button>
-    <div class="location-dropdown">
-      <!--{{#if location.language}}-->
-      <span class="location-dropdown-label">
+    <transition name="fade">
+      <div v-if="toggle"
+           v-on-clickaway="onClickAway"
+           class="location-dropdown">
+        <!--{{#if location.language}}-->
+        <span class="location-dropdown-label">
         {{ $t("main.header.language") }}
       </span>
-      <SelectBoxIt :options="languages"
-                   v-model="$i18n.locale"
-                   id="language"
-                   data-test="location-selector-dropdown"
-                   class="select location-select"/>
-      <!--{{/if}}-->
-      <!--{{#if location.country}}-->
-      <!--<form id="form-select-country" action="{{@root.meta._links.selectCountry.href}}" method="POST">-->
+        <SelectBoxIt :options="languages"
+                     v-model="$i18n.locale"
+                     id="language"
+                     data-test="location-selector-dropdown"
+                     class="select location-select"/>
+        <!--{{/if}}-->
+        <!--{{#if location.country}}-->
+        <!--<form id="form-select-country" action="{{@root.meta._links.selectCountry.href}}" method="POST">-->
         <!--<input type="hidden" name="csrfToken" value="{{@root.meta.csrfToken}}"/>-->
         <!--<span class="location-dropdown-label">{{i18n "main:header.country"}}</span>-->
         <!--<select name="country" id="country-select" class="select location-select">-->
-          <!--{{#each location.country}}-->
-          <!--<option value="{{value}}" {{#if selected}}selected{{/if}}>{{label}}</option>-->
-          <!--{{/each}}-->
+        <!--{{#each location.country}}-->
+        <!--<option value="{{value}}" {{#if selected}}selected{{/if}}>{{label}}</option>-->
+        <!--{{/each}}-->
         <!--</select>-->
-      <!--{{/if}}-->
-    </div>
+        <!--{{/if}}-->
+      </div>
+    </transition>
   </li>
 </template>
 
 <script>
+import VueClickaway from 'vue-clickaway';
+
 export default {
+  data: () => ({
+    toggle: false,
+  }),
+
   computed: {
     languages() {
       const configLangs = this.$sunrise.languages;
@@ -47,5 +56,13 @@ export default {
       return this.languages.length > 0;
     },
   },
+
+  methods: {
+    onClickAway() {
+      this.toggle = false;
+    },
+  },
+
+  mixins: [VueClickaway.mixin],
 };
 </script>
