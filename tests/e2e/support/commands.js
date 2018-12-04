08036f43d44319dfdd62c65efeaffe98d8444099
Laura Luiz <laura.luiz@commercetools.de>
2018-12-04 16:45:04 +0100

    Implement cypress commands to create and remove customers

--- tests/e2e/support/commands.js
+++ tests/e2e/support/commands.js
@@ -24,17 +24,66 @@
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
-Cypress.Commands.add('signup', (customer) => {
-  cy.visit('/login');
-  cy.get('[data-test=signup-form-firstname]').type(customer.firstName);
-  cy.get('[data-test=signup-form-lastname]').type(customer.lastName);
-  cy.get('[data-test=signup-form-email]').type(customer.email);
-  cy.get('[data-test=signup-form-password]').type(customer.password);
-  cy.get('[data-test=signup-form-repeatpassword]').type(customer.password);
-  cy.get('[data-test=signup-form-agreetoterms]').check();
-  cy.get('[data-test=signup-form-submit]').click();
+import gql from 'graphql-tag';
+import createClient from './test-apollo';
+
+const clientPromise = createClient();
+
+Cypress.Commands.add('login', customer =>
+  cy.createCustomer(customer).then(() => {
+    cy.visit('/login');
+    cy.get('[data-test=login-button]').click();
+    cy.get('[data-test=login-form-email]').type(customer.email);
+    cy.get('[data-test=login-form-password]').type(customer.password);
+    cy.get('[data-test=login-form-submit]').click();
+  }));
+
+Cypress.Commands.add('createCustomer', (draft) => {
+  const createCustomer = client => client.mutate({
+    mutation: gql`
+        mutation createNewCustomer($draft: CustomerSignMeUpDraft!) {
+          customerSignMeUp(draft: $draft) {
+            customer {
+              id
+            }
+          }
+        }`,
+    variables: { draft },
+  });
+
+  return cy.deleteCustomer(draft).then(() => cy.wrap(clientPromise.then(client => createCustomer(client))));
 });
 
-Cypress.Commands.add('logout', () => {
-  cy.get('[data-test=logout-button]').click();
+Cypress.Commands.add('deleteCustomer', ({ email }) => {
+  const deleteCustomer = client => client.query({
+    query: gql`
+      query queryCustomerByEmail($predicate: String) {
+        customers(limit: 1, where: $predicate) {
+          results {
+            id
+            version
+          }
+        }
+      }`,
+    variables: { predicate: `email = "${email}"` },
+    fetchPolicy: 'network-only',
+  }).then(async (response) => {
+    const customer = response.data.customers.results[0];
+    if (customer) {
+      await client.mutate({
+        mutation: gql`
+            mutation deleteOldCustomer($id: String!, $version: Long!) {
+              deleteCustomer(id: $id, version: $version, personalDataErasure: true) {
+                id
+              }
+            }`,
+        variables: {
+          id: customer.id,
+          version: customer.version,
+        },
+      }).catch(e => console.error('Customer already deleted', e));
+    }
+  });
+
+  return cy.wrap(clientPromise.then(client => deleteCustomer(client)));
 });
--- tests/e2e/support/utils.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-
-export const randomCustomer = () => ({
-  firstName: 'Charlie',
-  lastName: 'Bucket',
-  email: `charlie.bucket.${Cypress.moment().format('YYYYMMDDHHmmssSS')}@commercetools.com`,
-  password: 'p@ssword',
-});
