2d1f49e118583972c0ff5e5bcf71941605996a43
Zeina Idris <zeina.idris@commercetools.de>
2019-02-08 11:59:51 +0100

    Disable eslint error

--- src/auth.js
+++ src/auth.js
@@ -12,6 +12,7 @@ try {
     store.dispatch('setAuthenticated', true);
   }
 } catch (error) {
+  // eslint-disable-next-line no-console
   console.error('Could not retrieve token from local storage', error);
 }
 
