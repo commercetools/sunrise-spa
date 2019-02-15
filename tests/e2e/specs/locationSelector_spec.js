b2587cbc76cfc4ef1c54a089e3f1b613d5ecc9d0
Laura Luiz <laura.luiz@commercetools.de>
2019-02-15 12:35:33 +0100

    Add dynamic data to location selector e2e test

--- tests/e2e/specs/locationSelector_spec.js
+++ tests/e2e/specs/locationSelector_spec.js
@@ -2,10 +2,11 @@ describe('Location selector', () => {
   context('with initial DE', () => {
     beforeEach(() => {
       localStorage.setItem('locale', 'de');
-      cy.visit('/');
+      cy.visit('/product/lemare-booties-0778-grey/M0E20000000E0WX');
     });
 
     it('changes text to EN', () => {
+      cy.get('[data-test=product-name]').should('contain', 'Stiefeletten Lemare grau');
       cy.get('[data-test=stores-link]').should('contain', 'Filiale');
 
       cy.get('[data-test=location-selector-open-button]').click();
@@ -18,6 +19,7 @@ describe('Location selector', () => {
           expect(localStorage.getItem('locale')).to.eq('en');
         });
 
+      cy.get('[data-test=product-name]').should('contain', 'Booties Lemare grey');
       cy.get('[data-test=stores-link]').should('contain', 'Stores');
 
       cy.get('span[data-test=location-selector-dropdown]')
