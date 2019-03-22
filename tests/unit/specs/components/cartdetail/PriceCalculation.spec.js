d361d854daa772c8bc2ad698f65dad511cb916a0
Alex Plaskevich <plaskevich@gmail.com>
2019-03-22 18:20:15 +0100

    fix unit test

--- tests/unit/specs/components/cartdetail/PriceCalculation.spec.js
+++ tests/unit/specs/components/cartdetail/PriceCalculation.spec.js
@@ -2,14 +2,12 @@ import { shallowMount } from '@vue/test-utils';
 import PriceCalculation from '@/components/cartdetail/PriceCalculation.vue';
 
 describe('PriceCalculation.vue', () => {
-  const me = {
-    activeCart: {
-      lineItems: [],
-      totalPrice: {
-        currencyCode: 'EUR',
-        centAmount: 83050,
-        fractionDigits: 2,
-      },
+  const prices = {
+    lineItems: [],
+    totalPrice: {
+      currencyCode: 'EUR',
+      centAmount: 83050,
+      fractionDigits: 2,
     },
   };
 
@@ -19,6 +17,7 @@ describe('PriceCalculation.vue', () => {
     options = {
       methods: { formatPrice: jest.fn() },
       mocks: { $t: jest.fn() },
+      propsData: { prices },
     };
   });
 
@@ -28,24 +27,21 @@ describe('PriceCalculation.vue', () => {
 
   it('calculates applied taxes', () => {
     const wrapper = shallowMount(PriceCalculation, options);
-    wrapper.setData({ me });
     expect(wrapper.vm.taxes).toBeNull();
 
-    wrapper.setData({
-      me: {
-        activeCart: {
-          ...me.activeCart,
-          taxedPrice: {
-            totalNet: {
-              currencyCode: 'EUR',
-              centAmount: 69790,
-              fractionDigits: 2,
-            },
-            totalGross: {
-              currencyCode: 'EUR',
-              centAmount: 83050,
-              fractionDigits: 2,
-            },
+    wrapper.setProps({
+      prices: {
+        ...prices,
+        taxedPrice: {
+          totalNet: {
+            currencyCode: 'EUR',
+            centAmount: 69790,
+            fractionDigits: 2,
+          },
+          totalGross: {
+            currencyCode: 'EUR',
+            centAmount: 83050,
+            fractionDigits: 2,
           },
         },
       },
@@ -59,23 +55,20 @@ describe('PriceCalculation.vue', () => {
 
   it('calculates subtotal price', () => {
     const wrapper = shallowMount(PriceCalculation, options);
-    wrapper.setData({ me });
     expect(wrapper.vm.subtotal).toEqual({
       centAmount: 0,
       currencyCode: 'EUR',
       fractionDigits: 2,
     });
 
-    wrapper.setData({
-      me: {
-        activeCart: {
-          ...me.activeCart,
-          lineItems: [
-            { totalPrice: { centAmount: 11111 } },
-            { totalPrice: { centAmount: 22222 } },
-            { totalPrice: { centAmount: 44444 } },
-          ],
-        },
+    wrapper.setProps({
+      prices: {
+        ...prices,
+        lineItems: [
+          { totalPrice: { centAmount: 11111 } },
+          { totalPrice: { centAmount: 22222 } },
+          { totalPrice: { centAmount: 44444 } },
+        ],
       },
     });
     expect(wrapper.vm.subtotal).toEqual({
