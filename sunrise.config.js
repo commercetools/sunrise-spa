04a7b8f255bf7ff592b035600cf51adebadab77b
Laura Luiz <laura.luiz@commercetools.de>
2019-07-11 16:03:07 +0200

    remove unnecessary (for now) permission

--- sunrise.config.js
+++ sunrise.config.js
@@ -9,7 +9,7 @@ export default {
       },
       scopes: [process.env.VUE_APP_CT_SCOPE || 'manage_my_profile:sunrise-spa create_anonymous_token:sunrise-spa'
       + ' manage_my_payments:sunrise-spa view_products:sunrise-spa manage_my_orders:sunrise-spa'
-      + ' manage_my_shopping_lists:sunrise-spa view_project_settings:sunrise-spa'],
+      + ' manage_my_shopping_lists:sunrise-spa'],
     },
     api: process.env.VUE_APP_CT_API_HOST || 'https://api.sphere.io',
   },
