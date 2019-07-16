896a2f9dd4d0c75929b34f1e6875995b64cff91d
Alex Plaskevich <plaskevich@gmail.com>
2019-07-16 12:15:43 +0200

    fix orderDetail

--- src/components/useraccount/myorders/OrderDetail.vue
+++ src/components/useraccount/myorders/OrderDetail.vue
@@ -145,6 +145,9 @@ export default {
                       }
                     }
                   }
+                discountedPricePerQuantity {
+                  quantity
+                }
                 totalPrice {
                   ...DisplayableMoney
                 }
