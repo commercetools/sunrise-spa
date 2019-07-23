d629ffe5f03e81950e2ecd7bfb82bc56a6ebaa38
Laura Luiz <laura.luiz@commercetools.de>
2019-07-23 14:43:10 +0200

    Fix my account style by giving its class a unique name

--- src/components/useraccount/PageUserAccount.vue
+++ src/components/useraccount/PageUserAccount.vue
@@ -6,7 +6,7 @@
       </div>
       <div class="row my-account-content">
         <SidebarMenu class="col-sm-3 col-md-2 col-xs-12 my-account-sidebar"/>
-        <router-view class="col-sm-9 col-md-10 col-xs-12 checkout-form-step"/>
+        <router-view class="col-sm-9 col-md-10 col-xs-12 my-account-tab"/>
       </div>
     </div>
   </div>
