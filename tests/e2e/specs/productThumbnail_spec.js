9ad885013c59b9ac707e47fcd83d28a4e675e418
Harm Meijer <harm.meijer@commercetools.de>
2020-01-24 12:07:02 +0100

    price has changed (or has it?)

--- tests/e2e/specs/productThumbnail_spec.js
+++ tests/e2e/specs/productThumbnail_spec.js
@@ -20,11 +20,11 @@ describe('Product thumbnail', () => {
 
         cy.wrap($thumbnail)
           .find('[data-test=price-old-value]')
-          .contains(/^\s*248,75\s€\s*$/);
+          .contains(/^\s*199,00\s€\s*$/);
 
         cy.wrap($thumbnail)
           .find('[data-test=price-new-value]')
-          .contains(/^\s*174,12\s€\s*$/);
+          .contains(/^\s*139,30\s€\s*$/);
       });
 
     cy.get('[data-test=product-thumbnail-name]')
@@ -38,7 +38,7 @@ describe('Product thumbnail', () => {
 
         cy.wrap($thumbnail)
           .find('[data-test=product-original-price]')
-          .contains(/\s*231,25\s€\s*$/);
+          .contains(/\s*185,00\s€\s*$/);
 
         cy.wrap($thumbnail)
           .find('[data-test=price-new-value]')
