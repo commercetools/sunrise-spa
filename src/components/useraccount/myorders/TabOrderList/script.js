fe1e98b68dad82d385073856e25f677e8454708f
Alex Plaskevich <plaskevich@gmail.com>
2020-02-06 23:17:50 +0100

    implement pages in orders

--- src/components/useraccount/myorders/TabOrderList/script.js
+++ src/components/useraccount/myorders/TabOrderList/script.js
@@ -5,21 +5,45 @@ import LoadingSpinner from '../../../common/LoadingSpinner/index.vue';
 import MONEY_FRAGMENT from '../../../Money.gql';
 
 export default {
-  components: { BaseMoney, BaseDate, LoadingSpinner },
+  components: {
+    BaseMoney, BaseDate, LoadingSpinner,
+  },
   data: () => ({
     me: null,
+    page: 0,
+    limit: 3,
   }),
   computed: {
+    ordersPerPage() {
+      return this.me?.orders?.results.slice(this.page * this.limit, this.page * this.limit + this.limit);
+    },
     isLoading() {
       return this.$apollo.loading;
     },
     orderListNotEmpty() {
       return this.me?.orders?.results.length > 0;
     },
+    totalPages() {
+      return Math.ceil(this.me?.orders?.results.length / this.limit);
+    },
+
+    isInFirstPage() {
+      return this.page === 0;
+    },
+    isInLastPage() {
+      return this.page >= this.me?.orders?.results.length / this.limit - 1;
+    },
+
   },
   methods: {
     translateStatus(state) {
       return state ? this.$t(state) : '-';
+    },
+    pageForward() {
+      this.page += 1;
+    },
+    pageBack() {
+      this.page -= 1;
     },
   },
   apollo: {
