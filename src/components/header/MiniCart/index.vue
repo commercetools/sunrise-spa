785986b9ae76d4df99aa11bb2e1d309c35216a92
Harm Meijer <harm.meijer@commercetools.de>
2020-04-22 17:25:11 +0200

    ok

--- src/components/header/MiniCart/index.vue
+++ src/components/header/MiniCart/index.vue
@@ -1,4 +1,4 @@
 <template src="./template.html"></template>
-<style src="./style.css"></style>
+<style src="./style.scss" lang="scss" scoped></style>
 <i18n src="./i18n.txt"></i18n>
 <script src="./script.js"></script>
