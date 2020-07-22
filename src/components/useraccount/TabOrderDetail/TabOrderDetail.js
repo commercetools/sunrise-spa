253fc627041290ac3fb6449abd0257df1f8bb6eb
Alex Plaskevich <plaskevich@gmail.com>
2020-07-22 17:51:19 +0200

    my orders

--- src/components/useraccount/TabOrderDetail/TabOrderDetail.js
+++ src/components/useraccount/TabOrderDetail/TabOrderDetail.js
@@ -1,6 +1,7 @@
 import gql from 'graphql-tag';
 import BaseDate from '../../common/BaseDate/BaseDate.vue';
 import BaseAddress from '../../common/BaseAddress/BaseAddress.vue';
+import BaseMoney from '../../common/BaseMoney/BaseMoney.vue';
 import LineItemInfo from '../../common/cartlike/LineItemInfo/LineItemInfo.vue';
 import CartLikeContentDetail from '../../common/cartlike/CartLikeContentDetail/CartLikeContentDetail.vue';
 import ORDER_FRAGMENT from '../../Order.gql';
@@ -12,12 +13,26 @@ export default {
   components: {
     CartLikeContentDetail,
     BaseDate,
+    BaseMoney,
     BaseAddress,
     LineItemInfo,
   },
   data: () => ({
     me: null,
   }),
+  computed: {
+    subtotal() {
+      if (this.me) {
+        const { currencyCode, fractionDigits } = this.me.order.totalPrice;
+        return {
+          centAmount: this.me.order.lineItems.reduce((acc, li) => acc + li.totalPrice.centAmount, 0),
+          currencyCode,
+          fractionDigits,
+        };
+      }
+      return null;
+    },
+  },
   apollo: {
     me: {
       query: gql`
