4c7887232a105c405e3217decb027f0a0f162179
Alex Plaskevich <45790929+plaskevich@users.noreply.github.com>
2020-10-29 17:40:02 +0100

    merge

---
+++ src/components/common/CartLike/DiscountCodes/DiscountCodes.vue
@@ -0,0 +1,30 @@
+<style src="./style.css" scoped></style>
+<i18n src="./DiscountCodes.txt"></i18n>
+<script src="./DiscountCodes.js"></script>
+
+<template>
+  <div class="single-grand-total">
+    <div class="single-grand-total-left col-sm-6">
+      <span>{{ $t('appliedDiscounts') }}</span>
+    </div>
+    <div
+      v-for="discountInfo in cartLike.discountCodes"
+      :key="discountInfo.discountCode.id"
+      class="single-grand-total-right col-sm-6"
+      data-test="discount-code-name"
+    >
+      <b>
+        {{ discountInfo.discountCode.code }}
+      </b>
+      <span v-if="discountInfo.discountCode.name">
+        ({{ discountInfo.discountCode.name }})</span
+      >
+      <span>
+        <RemoveDiscountCodeForm
+          v-if="editable"
+          :codeId="discountInfo.discountCode.id"
+        />
+      </span>
+    </div>
+  </div>
+</template>
