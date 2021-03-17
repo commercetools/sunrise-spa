7536dc1470d68f466cdda676b4f4baca3befe5d7
Danu Mahendra <danu.m@photoninfotech.net>
2021-03-17 09:24:26 +0700

    Update sunrise.config.js

--- sunrise.config.js
+++ sunrise.config.js
@@ -1,121 +1,111 @@
 // eslint-disable-next-line no-console
 console.log("using host:", process.env.VUE_APP_CT_API_HOST);
-let localConfig = {};
-if(process.env.VUE_APP_LOCAL_SUNRISE_CONFIG){
-  localConfig = require(process.env.VUE_APP_LOCAL_SUNRISE_CONFIG).default;
-}
 
 export default {
-  ct: {
-    auth: {
-      host:
-        process.env.VUE_APP_CT_AUTH_HOST ||
-        "https://auth.europe-west1.gcp.commercetools.com",
-      projectKey:
-        process.env.VUE_APP_CT_PROJECT_KEY || "sunrise-spa",
-      credentials: {
-        clientId:
-          process.env.VUE_APP_CT_CLIENT_ID ||
-          "jFVHj0-tO-THQt9evnGTJ2fD",
-        clientSecret:
-          process.env.VUE_APP_CT_CLIENT_SECRET ||
-          "eUQgmtanysDpYxlOePOhcFklrwa5X8Sj",
-      },
-      scopes: [
-        process.env.VUE_APP_CT_SCOPE ||
-          "manage_my_profile:sunrise-spa create_anonymous_token:sunrise-spa" +
-            " manage_my_payments:sunrise-spa view_products:sunrise-spa manage_my_orders:sunrise-spa" +
-            " manage_my_shopping_lists:sunrise-spa",
-      ],
-    },
-    api:
-      process.env.VUE_APP_CT_API_HOST ||
-      "https://api.europe-west1.gcp.commercetools.com",
-  },
-  languages: {
-    en: "English",
-    de: "Deutsch",
-  },
-  countries: {
-    DE: "Deutschland",
-    US: "United States",
-  },
-  formats: {
-    number: {
-      DE: {
-        currency: {
-          style: "currency",
-          currency: "EUR",
-          currencyDisplay: "symbol",
+    ct: {
+        auth: {
+            host:
+                process.env.VUE_APP_CT_AUTH_HOST ||
+                "https://auth.us-central1.gcp.commercetools.com",
+            projectKey:
+                process.env.VUE_APP_CT_PROJECT_KEY || "photon-learning",
+            credentials: {
+                clientId:
+                    process.env.VUE_APP_CT_CLIENT_ID ||
+                    "QKE1n_hvGjwMAMw88Iv3XyxK",
+                clientSecret:
+                    process.env.VUE_APP_CT_CLIENT_SECRET ||
+                    "bNOWIy7H6jZOHi7xNE5teKqr4RkCOi0m",
+            },
+            scopes: [
+                process.env.VUE_APP_CT_SCOPE || "manage_project:photon-learning",
+            ],
         },
-      },
-      US: {
-        currency: {
-          style: "currency",
-          currency: "USD",
+        api:
+            process.env.VUE_APP_CT_API_HOST ||
+            "https://api.us-central1.gcp.commercetools.com",
+    },
+    languages: {
+        en: "English",
+        id: "Indonesia"
+    },
+    countries: {
+        US: "United States",
+        ID: "Indonesia"
+    },
+    formats: {
+        number: {
+            US: {
+                currency: {
+                    style: "currency",
+                    currency: "USD",
+                },
+            },
+            IDR: {
+                currency: {
+                    style: "currency",
+                    currency: "IDR",
+                },
+            },
         },
-      },
-    },
-    datetime: {
-      US: {
-        short: {
-          year: "numeric",
-          month: "short",
-          day: "numeric",
+        datetime: {
+            ID: {
+                short: {
+                    year: "numeric",
+                    month: "short",
+                    day: "numeric",
+                },
+            },
+            US: {
+                short: {
+                    year: "numeric",
+                    month: "short",
+                    day: "numeric",
+                },
+            },
         },
-      },
-      DE: {
-        short: {
-          year: "numeric",
-          month: "short",
-          day: "numeric",
+    },
+    categories: {
+        salesExternalId: "8",
+    },
+    facetSearches: [
+        {
+            name: "size",
+            type: "text",
+            label: {
+                it: "Size",
+                en: "Size",
+            },
         },
-      },
-    },
-  },
-  categories: {
-    salesExternalId: "6",
-  },
-  facetSearches: [
-    {
-      name: "size",
-      type: "text",
-      label: {
-        it: "Size",
-        de: "Größe",
-        en: "Size",
-      },
-    },
-    {
-      name: "color",
-      type: "lnum",
-      component: "colors",
-      label: {
-        de: "Farbe",
-        it: "Color",
-        en: "Color",
-      },
-    },
-    {
-      name: "designer",
-      type: "enum",
-      component: "designer",
-      label: {
-        it: "Designer",
-        de: "Designer",
-        en: "Designer",
-      },
-    },
-  ],
-  detailAttributes: [
-    "designer",
-    "colorFreeDefinition",
-    "size",
-    "style",
-    "gender",
-    "articleNumberManufacturer",
-  ],
-  variantSelector: ["color", "size"],
-  variantInProductName: ["size"],
-  ...localConfig
+        {
+            name: "color",
+            type: "enum",
+            component: "color",
+            label: {    
+                it: "Color",
+                en: "Color",
+            },
+        },
+        {
+            name: "choosegender",
+            type: "enum",
+            component: "choosegender",
+            label: {
+                it: "Jenis Kelamin",
+                en: "Gender",
+            },
+        },
+    ],
+    detailAttributes: [
+        "productcolor",
+        "designer",
+        "colorFreeDefinition",
+        "size",
+        "brand",
+        "choosegender",
+        "articleNumberManufacturer",
+        "productDescription"
+    ],
+    variantSelector: ["color", "size"],
+    variantInProductName: ["size"],
 };
