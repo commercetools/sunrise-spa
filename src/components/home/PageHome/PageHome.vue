140d5687f6fca78ebc9928655f69d5e2143cce47
Danu Mahendra <danu.m@photoninfotech.net>
2021-03-17 17:02:35 +0700

    update page home

--- src/components/home/PageHome/PageHome.vue
+++ src/components/home/PageHome/PageHome.vue
@@ -16,11 +16,11 @@
           >
             <div class="banner-img banner-hover banner-zoom">
               <router-link
-                :to="{ name: 'products', params: { categorySlug: 'women-clothing-jeans' } }"
+                :to="{ name: 'products', params: { categorySlug: 'Sneakers' } }"
               >
                 <img src="assets/img/jeans1.jpg" alt="banner" />
                 <div class="banner-content">
-                  <h3>SNEAKERS</h3>
+                  <h3>WOMAN SNEAKERS</h3>
                 </div>
               </router-link>
             </div>
@@ -34,11 +34,11 @@
           >
             <div class="banner-img banner-hover banner-zoom">
               <router-link
-                :to="{ name: 'products', params: { categorySlug: 'men-shoes-sneakers' } }"
+                :to="{ name: 'products', params: { categorySlug: 'running' } }"
               >
                 <img src="assets/img/sho3.jpg" alt="banner" />
                 <div class="banner-content">
-                  <h3>RUNNING</h3>
+                  <h3>MAN RUNNING SHOES</h3>
                 </div>
               </router-link>
             </div>
@@ -50,7 +50,7 @@
           >
             <div class="banner-img banner-hover banner-zoom">
               <router-link
-                :to="{ name: 'products', params: { categorySlug: 'accessories' } }"
+                :to="{ name: 'products', params: { categorySlug: 'man-soccer' } }"
               >
                 <img src="assets/img/acc.jpg" alt="banner" />
                 <div class="banner-content">
@@ -68,11 +68,11 @@
           >
             <div class="banner-img banner-hover banner-zoom">
               <router-link
-                :to="{ name: 'products', params: { categorySlug: 'women-clothing-swimwear' } }"
+                :to="{ name: 'products', params: { categorySlug: 'kids' } }"
               >
                 <img src="assets/img/s3.jpg" alt="banner" />
                 <div class="banner-content">
-                  <h3>KIDS</h3>
+                  <h3>KIDS SHOES</h3>
                 </div>
               </router-link>
             </div>
