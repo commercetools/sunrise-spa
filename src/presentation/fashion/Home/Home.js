d83de1e09d7804f38a95756e43046d7476406fc0
Harm Meijer <harm.meijer@commercetools.com>
2022-05-09 17:27:55 +0200

    no mibile menu, just make products all the homepage

--- src/presentation/fashion/Home/Home.js
+++ src/presentation/fashion/Home/Home.js
@@ -1,14 +1,20 @@
 import { useI18n } from 'vue-i18n';
 import Banner from 'presentation/Banner/Banner.vue';
 import BaseMoney from 'presentation/components/BaseMoney/BaseMoney.vue';
+import { useRouter } from 'vue-router';
 
 export default {
   name: 'Home',
   components: { Banner, BaseMoney },
 
   setup() {
-    //cannot use i18n tag and working number format from setup in the same
-    //  component (vue is great!!)
+    const router = useRouter();
+    if (window.innerWidth < 990) {
+      router.replace({
+        name: 'products',
+        params: { categorySlug: 'all' },
+      });
+    }
     const { t } = useI18n();
     return {
       t,
